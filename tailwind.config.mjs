93f430aad815f0edc0d18d9a258fb388ce7571b1
tmtz <metzias@t-online.de>
2024-01-28 18:03:22 +0100

    inital Commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        mega: "#0009ad",
+      },
+    },
+  },
+  plugins: [
+    // ...
+    require("@tailwindcss/forms"),
+  ],
+};
